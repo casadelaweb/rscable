9949232776c4f2797c805959a1250f1cda67cd78
Alex Volkov <a.volkov@3l.ru>
2024-04-29 16:18:53 +0300

    refactor: update catalog filters

--- public/views/pages/catalog.php
+++ public/views/pages/catalog.php
@@ -47,6 +47,253 @@ $suggestions = [
 
 <?php Render::component('breadcrumbs') ?>
 
+<form class="catalog-filters" data-catalog="filters" data-modal="catalog-filters">
+  <!-- кнопки применить и сбросить ссылаются на эти поля -->
+  <input id="catalog-reset" hidden type="reset">
+  <input id="catalog-submit" hidden type="submit">
+
+  <!-- только для мобильной версии -->
+  <div class="catalog-filters-header">
+    <span>Фильтры</span>
+    <button class="catalog-filters-close" data-modal-close title="Закрыть окно" type="button">
+      <span class="iconfont icon-close"></span>
+    </button>
+  </div>
+
+  <div class="catalog-filters-body">
+    <div class="catalog-filter _category">
+      <div class="catalog-filter-summary">
+        <div class="catalog-filter-summary-content">
+          <span>Категории</span>
+          <button class="catalog-filter-button" title="Открыть фильтр" type="button">
+            <span
+              class="iconfont icon-arrow-down"></span>
+          </button>
+        </div>
+        <span class="catalog-filter-selected"></span>
+      </div>
+      <div class="catalog-filter-body">
+        <label class="catalog-filter-label">
+          <input name="category" type="checkbox">
+          <span>Платья</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="category" type="checkbox">
+          <span>Нарядная одежда</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="category" type="checkbox">
+          <span>Блузы и рубашки</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="category" type="checkbox">
+          <span>Брюки и юбки</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="category" type="checkbox">
+          <span>Верхняя одежда</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="category" type="checkbox">
+          <span>Джемперы и кардиганы</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="category" type="checkbox">
+          <span>Костюмы и жакеты</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="category" type="checkbox">
+          <span>Футболки и топы</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="category" type="checkbox">
+          <span>Аксессуары</span>
+        </label>
+        <label class="catalog-filters-submit" for="catalog-submit">
+          <span>Применить</span>
+        </label>
+      </div>
+    </div>
+    <div class="catalog-filter _sorting">
+      <div class="catalog-filter-summary">
+        <div class="catalog-filter-summary-content">
+          <span>Сортировать</span>
+          <button class="catalog-filter-button" title="Открыть фильтр" type="button">
+            <span class="iconfont icon-arrow-down"></span>
+          </button>
+        </div>
+        <span class="catalog-filter-selected"></span>
+      </div>
+      <div class="catalog-filter-body">
+        <label class="catalog-filter-label">
+          <input name="sorting" type="radio" value="newest">
+          <span> По новизне</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="sorting" type="radio" value="popular">
+          <span> По популярности</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="sorting" type="radio" value="cheapest">
+          <span> По возрастанию цены</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="sorting" type="radio" value="expensive">
+          <span> По убыванию цены</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="sorting" type="radio" value="discount">
+          <span> Товары по скидке</span>
+        </label>
+        <label class="catalog-filters-submit" for="catalog-submit">
+          <span>Применить</span>
+        </label>
+      </div>
+    </div>
+    <div class="catalog-filter">
+      <div class="catalog-filter-summary">
+        <div class="catalog-filter-summary-content">
+          <span>Размер</span>
+          <button class="catalog-filter-button" title="Открыть фильтр" type="button">
+            <span class="iconfont icon-arrow-down"></span>
+          </button>
+        </div>
+        <span class="catalog-filter-selected"></span>
+      </div>
+      <div class="catalog-filter-body">
+        <label class="catalog-filter-label">
+          <input name="sizes" type="checkbox" value="XS">
+          <span> XS</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="sizes" type="checkbox" value="S">
+          <span> S</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="sizes" type="checkbox" value="M">
+          <span> M</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="sizes" type="checkbox" value="L">
+          <span> L</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="sizes" type="checkbox" value="XL">
+          <span> XL</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="sizes" type="checkbox" value="XXL">
+          <span> XXL</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="sizes" type="checkbox" value="3XL">
+          <span> 3XL</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="sizes" type="checkbox" value="4XL">
+          <span> 4XL</span>
+        </label>
+        <label class="catalog-filters-submit" for="catalog-submit">
+          <span>Применить</span>
+        </label>
+      </div>
+    </div>
+    <div class="catalog-filter">
+      <div class="catalog-filter-summary">
+        <div class="catalog-filter-summary-content">
+          <span>Цена</span>
+          <button class="catalog-filter-button" title="Открыть фильтр" type="button">
+            <span class="iconfont icon-arrow-down"></span>
+          </button>
+        </div>
+        <div class="catalog-filter-selected" data-select="no-overwrite">
+          от
+          <span id="span-price-min"></span>
+          ₽
+          до
+          <span id="span-price-max"></span>
+          ₽
+        </div>
+      </div>
+      <div class="catalog-filter-body">
+        <div class="select-prices">
+          <input id="input-price-min" class="select-input" name="price_min" type="text">
+          <input id="input-price-max" class="select-input" name="price_max" type="text">
+        </div>
+        <div data-catalog-prices
+             data-input-max="#input-price-max"
+             data-input-min="#input-price-min"
+             data-max="12999"
+             data-min="199"
+             data-span-max="#span-price-max"
+             data-span-min="#span-price-min"></div>
+        <label class="catalog-filters-submit" for="catalog-submit">
+          <span>Применить</span>
+        </label>
+      </div>
+    </div>
+    <div class="catalog-filter">
+      <div class="catalog-filter-summary">
+        <div class="catalog-filter-summary-content">
+          <span>Цвет</span>
+          <button class="catalog-filter-button" title="Открыть фильтр" type="button">
+            <span class="iconfont icon-arrow-down"></span>
+          </button>
+        </div>
+        <span class="catalog-filter-selected"></span>
+      </div>
+      <div class="catalog-filter-body">
+        <label class="catalog-filter-label">
+          <input name="color" type="checkbox" value="Белый и золотой">
+          <span> Белый и золотой</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="color" type="checkbox" value="Белый и молочный">
+          <span> Белый и молочный</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="color" type="checkbox" value="Черный">
+          <span> Черный</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="color" type="checkbox" value="Бежевый и золотой">
+          <span> Бежевый и золотой</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="color" type="checkbox" value="Зеленый и изумрудный">
+          <span> Зеленый и изумрудный</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="color" type="checkbox" value="Красный и бордовый">
+          <span>Красный и бордовый</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="color" type="checkbox" value="Оливковый и хаки">
+          <span> Оливковый и хаки</span>
+        </label>
+        <label class="catalog-filter-label">
+          <input name="color" type="checkbox" value="Коричневый и шоколадный">
+          <span> Коричневый и шоколадный</span>
+        </label>
+        <label class="catalog-filters-submit" for="catalog-submit">
+          <span>Применить</span>
+        </label>
+      </div>
+    </div>
+  </div>
+
+  <!-- только для мобильной версии -->
+  <div class="catalog-filters-footer">
+    <label class="catalog-filters-reset" for="catalog-reset">
+      <span>Сбросить</span>
+    </label>
+    <label class="catalog-filters-submit" for="catalog-submit">
+      <span>Применить</span>
+    </label>
+  </div>
+</form>
+
+
 <div class="catalog">
   <div class="catalogContainer">
     <div class="catalogHeader">
--- src/components/pages/catalog/catalog.ts
+++ src/components/pages/catalog/catalog.ts
@@ -1,4 +1,98 @@
+import * as noUiSlider from 'nouislider'
+import { API } from 'nouislider/dist/nouislider'
+import { Select } from 'src/modules/select/select'
+import { Details } from 'src/modules/details/details'
+
+interface sliderElement extends HTMLElement {
+  noUiSlider: API
+}
+
+function preparePrice(value: string): number {
+  return parseInt(value)
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+  const { body, } = document
+
+  const pricesSlider: sliderElement = body.querySelector('[data-catalog-prices]')
+
+  if (pricesSlider) {
+    const priceMin = preparePrice(pricesSlider.getAttribute('data-min'))
+    const priceMax = preparePrice(pricesSlider.getAttribute('data-max'))
+    const inputMin: HTMLInputElement = body.querySelector(pricesSlider.getAttribute('data-input-min'))
+    const inputMax: HTMLInputElement = body.querySelector(pricesSlider.getAttribute('data-input-max'))
+    const spanMin: HTMLElement = body.querySelector(pricesSlider.getAttribute('data-span-min'))
+    const spanMax: HTMLElement = body.querySelector(pricesSlider.getAttribute('data-span-max'))
+
+    let typingTimerMin: ReturnType<typeof setTimeout>
+    let typingTimerMax: ReturnType<typeof setTimeout>
+    const doneTypingInterval: number = 750
+    inputMin.addEventListener('keyup', () => {
+      clearTimeout(typingTimerMin)
+      if (inputMin.value) {
+        typingTimerMin = setTimeout(() => {
+          pricesSlider.noUiSlider.set([inputMin.value, null,])
+        }, doneTypingInterval)
+      }
+    })
+    inputMax.addEventListener('keyup', () => {
+      clearTimeout(typingTimerMax)
+      if (inputMax.value) {
+        typingTimerMax = setTimeout(() => {
+          pricesSlider.noUiSlider.set([null, inputMax.value,])
+        }, doneTypingInterval)
+      }
+    })
+
+    noUiSlider.create(pricesSlider, {
+      start: [priceMin, priceMax,],
+      step: 1,
+      connect: true,
+      range: {
+        'min': priceMin,
+        'max': priceMax,
+      },
+    }).on('update', (values: string[], handle: 0 | 1) => {
+      const value = values[handle]
+      if (handle === 0) {
+        inputMin.value = parseInt(value).toLocaleString()
+      } else {
+        inputMax.value = parseInt(value).toLocaleString()
+      }
+
+      spanMin.textContent = parseInt(values[0]).toLocaleString()
+      spanMax.textContent = parseInt(values[1]).toLocaleString()
+    })
+  }
+
+  new Details({
+    preferButtonIfExist: true,
+    selectors: {
+      details: '.catalog-filter',
+      summary: '.catalog-filter-summary',
+      button: '.catalog-filter-button',
+      content: '.catalog-filter-body',
+      scrollbars: { vertical: 'has-vertical-scrollbar', },
+    },
+    onlyUnderLaptop: true,
+  }).init()
+  const catalogSelects = new Select({
+    selectors: {
+      select: '.catalog-filter',
+      current: '.catalog-filter-summary',
+      value: '.catalog-filter-selected',
+      button: '.catalog-filter-button',
+      options: '.catalog-filter-body',
+      option: '.catalog-filter-label',
+    },
+  })
+  catalogSelects.init()
+  const formFilters: HTMLElement = document.body.querySelector('[data-catalog=filters]')
+  if (formFilters) {
+    formFilters.addEventListener('reset', () => {
+      catalogSelects.updateSelects()
+    })
+  }
 }, {
   passive: true,
   once: true,
